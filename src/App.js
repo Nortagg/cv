1da30b018161386e4f0a6f4a068ad4e5da4f04ea
Nikola Lezaic <lezaicnikola10@gmail.com>
2023-05-04 14:48:51 +0200

    adjusted some sizing issues with margins auto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import CardAbout from "./Components/container-about/container-about";
 import CardProfile from "./Components/container-profile/container-profile";
 import HomePage from "./Components/home/home-page";
